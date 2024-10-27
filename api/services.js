a804b8197143dcb1ee5bd2762c723a84bf4f79b3
ThanhB <bintri92@gmail.com>
2024-10-27 22:24:24 +0700

    fix

--- api/services.js
+++ api/services.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-import axiosClient from 'axios';
+
 import {aget} from "../commons/util_axios";
 const getServices = async () => {
   try {
     const response = await aget('/services'); // Replace with your machine's IP address
-=======
-import axios from 'axios';
-
-const getServices = async () => {
-  try {
-    const response = await axios.get('http://192.168.88.163:5001/services'); // Replace with your machine's IP address
->>>>>>> 514ff2a7ea1d9ef0764ac763239fa825c00e1e72
     return response.data;
   } catch (error) {
     console.error('Error fetching services:', error);
